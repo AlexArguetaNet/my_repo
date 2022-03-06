6d9cea7017c1aacd578c8e95d63ad592012a0507
Alex <alexarguetanet@gmail.com>
2022-03-06 15:42:58 -0500

    Added dicstionaires.py

---
+++ practice/python/automate_boring/dictionaries.py
@@ -0,0 +1,7 @@
+# Dictionaries uses key-value pairs instead
+# of indeces to location values inside this
+# data structure
+
+birthdays = {'Alice': 'April 1', 'Bob': 'Dec 12', \
+                'Carol': 'March 4'}
+
